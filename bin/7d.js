1023fb1b46022b5b62d97df4f857b3634f10a001
Raoul Millais <raoul@raoulmillais.com>
2014-01-21 12:11:51 +0000

    Clean up logging

--- bin/7d.js
+++ bin/7d.js
@@ -3,15 +3,18 @@
 var cli = require('../lib/cli');
 var argv = require('optimist').argv;
 var api = require('../lib/api');
-var eyes = require('eyes');
 var metadata = cli.argv(argv);
+var inspect = require('eyes').inspector({
+	indent: 2,
+	maxLength: 4096
+});
 
 api.call(metadata, function (err, res) {
 	if (err) {
 		return cli.util.die(666, err);
 	}
 
-	eyes.inspect(res);
+	inspect(res);
 	process.exit(0);
 });
 
